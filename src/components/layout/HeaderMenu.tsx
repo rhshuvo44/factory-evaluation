bc86f059694f3d063f6730492337ffbb228f8a5e
Ripon Hossain Shuvo <rhshuvo44@gmail.com>
2024-09-15 16:13:38 +0600

    header

--- src/components/layout/HeaderMenu.tsx
+++ src/components/layout/HeaderMenu.tsx
@@ -2,15 +2,25 @@ import { Layout, Menu } from "antd";
 
 const { Header } = Layout;
 
-const items = new Array(3).fill(null).map((_, index) => ({
-  key: String(index + 1),
-  label: `nav ${index + 1}`,
-}));
+const items = [
+  {
+    key: "header",
+    label: "Home",
+    to: "/",
+  },
+];
 const HeaderMenu = () => {
+  const date = new Date();
   return (
-    <Header className="flex items-center">
+    <Header className="flex items-center gap-5">
       <div className="flex justify-content-center items-center text-white">
-        <h1 className="text-white font-bold lg:text-3xl">Sarkar Group</h1>
+        <h1 className="text-white font-bold md:text-2xl lg:text-3xl">Sarkar Group</h1>
+      </div>
+      <div className="flex items-center px-8 text-white justify-center">
+        <h3 className="font-bold md:text-2xl lg:text-3xl capitalize text-primary mr-3">
+          Hello Ripon,
+        </h3>
+        <p className="lg:mt-3"> {date.toDateString()}</p>
       </div>
       <Menu
         theme="dark"
@@ -18,6 +28,7 @@ const HeaderMenu = () => {
         defaultSelectedKeys={["2"]}
         items={items}
         style={{ flex: 1, minWidth: 0 }}
+        className="flex justify-end"
       />
     </Header>
   );
