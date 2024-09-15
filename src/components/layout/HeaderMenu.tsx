a4521d5db1da752e2d6448ef538ae995c15d6a13
Ripon Hossain Shuvo <rhshuvo44@gmail.com>
2024-09-15 16:37:29 +0600

    header with profile

--- src/components/layout/HeaderMenu.tsx
+++ src/components/layout/HeaderMenu.tsx
@@ -1,20 +1,33 @@
-import { Layout, Menu } from "antd";
-
+import { Avatar, Dropdown, Layout, MenuProps } from "antd";
+import userImg from "../../assets/image/user.jpg";
 const { Header } = Layout;
 
-const items = [
+const items: MenuProps["items"] = [
   {
-    key: "header",
-    label: "Home",
-    to: "/",
+    label: "Profile",
+    key: "profile",
+  },
+  {
+    label: "Settings",
+    key: "settings",
+  },
+
+  {
+    type: "divider",
+  },
+  {
+    label: "Logout",
+    key: "1",
   },
 ];
 const HeaderMenu = () => {
   const date = new Date();
   return (
-    <Header className="flex items-center gap-5">
+    <Header className="flex items-center gap-5 justify-between">
       <div className="flex justify-content-center items-center text-white">
-        <h1 className="text-white font-bold md:text-2xl lg:text-3xl">Sarkar Group</h1>
+        <h1 className="text-white font-bold md:text-2xl lg:text-3xl">
+          Sarkar Group
+        </h1>
       </div>
       <div className="flex items-center px-8 text-white justify-center">
         <h3 className="font-bold md:text-2xl lg:text-3xl capitalize text-primary mr-3">
@@ -22,14 +35,16 @@ const HeaderMenu = () => {
         </h3>
         <p className="lg:mt-3"> {date.toDateString()}</p>
       </div>
-      <Menu
-        theme="dark"
-        mode="horizontal"
-        defaultSelectedKeys={["2"]}
-        items={items}
-        style={{ flex: 1, minWidth: 0 }}
-        className="flex justify-end"
-      />
+
+      <Dropdown menu={{ items }} trigger={["click"]}>
+        <a onClick={(e) => e.preventDefault()}>
+          <Avatar
+            src={userImg}
+            alt="avatar"
+            size={{ xs: 24, sm: 32, md: 40, lg: 50, xl: 56, xxl: 60 }}
+          />
+        </a>
+      </Dropdown>
     </Header>
   );
 };
