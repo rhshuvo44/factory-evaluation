e21113093dcd665a390da0c706643ef0994d87d5
Ripon Hossain Shuvo <rhshuvo44@gmail.com>
2024-09-18 15:43:32 +0600

    Running cost table

--- src/components/Table/RunningCostTable.tsx
+++ src/components/Table/RunningCostTable.tsx
@@ -1,14 +1,11 @@
-import { Button, Table } from "antd";
-import { useGetTravellingsQuery } from "../../redux/api/api";
-import { TRunning } from "../../types/tableType";
-import Loading from "../ui/Loading";
-import { useNavigate } from "react-router-dom";
+import { Table } from "antd";
 import { useState } from "react";
+import { useGetTravellingsQuery } from "../../redux/api/api";
+import Loading from "../ui/Loading";
 
 const RunningCostTable = () => {
   const [currentPage, setCurrentPage] = useState(1);
   const [pageSize, setPageSize] = useState(5);
-  const navigate = useNavigate();
 
   const colums = [
     {
@@ -72,34 +69,25 @@ const RunningCostTable = () => {
       dataIndex: "total price",
       key: "total price",
     },
-    {
-      title: "Action",
-      key: "action",
-      render: (_: number, record: TRunning) => (
-        <Button type="link" onClick={() => navigate(`/product/${record.slNo}`)}>
-          View Details
-        </Button>
-      ),
-    },
   ];
-  const { data, isError, isLoading } = useGetTravellingsQuery({
-    limit: pageSize,
-    skip: (currentPage - 1) * pageSize,
-  });
+  // const { data, isError, isLoading } = useGetTravellingsQuery({
+  //   limit: pageSize,
+  //   skip: (currentPage - 1) * pageSize,
+  // });
 
-  if (isLoading) return <Loading />;
-  if (isError) return <div>Error: {isError}</div>;
+  // if (isLoading) return <Loading />;
+  // if (isError) return <div>Error: {isError}</div>;
   return (
     <Table
       className="table-auto"
       bordered
       columns={colums}
-      dataSource={data?.travelling}
+      // dataSource={data?.travelling}
       rowKey="id"
       pagination={{
         current: currentPage,
         pageSize: pageSize,
-        total: data?.total,
+        // total: data?.total,
         onChange: (page, pageSize) => {
           setCurrentPage(page);
           setPageSize(pageSize);
