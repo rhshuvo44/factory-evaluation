1a78ddfc6a762617f5cab6a52201218d7c132cae
Ripon Hossain Shuvo <rhshuvo44@gmail.com>
2024-09-18 14:24:47 +0600

    Fixed Cost Add form Ddded

--- src/components/Table/FixedCostTable.tsx
+++ src/components/Table/FixedCostTable.tsx
@@ -1,11 +1,5 @@
-import React from 'react';
-
 const FixedCostTable = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return <div></div>;
 };
 
 export default FixedCostTable;
