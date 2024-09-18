38d10d78b22eb613938e6e4a67690878e9930924
Ripon Hossain Shuvo <rhshuvo44@gmail.com>
2024-09-18 10:38:16 +0600

    Factory development cost table

--- src/components/Table/FactoryDevelopmentTable.tsx
+++ src/components/Table/FactoryDevelopmentTable.tsx
@@ -1,5 +1,110 @@
+import { Button, Table } from "antd";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { TFactory } from "../../types/tableType";
+
 const FactoryDevelopmentTable = () => {
-  return <div></div>;
+  const [currentPage, setCurrentPage] = useState(1);
+  const [pageSize, setPageSize] = useState(5);
+  const navigate = useNavigate();
+
+  const colums = [
+    {
+      title: "SL No",
+      dataIndex: "SL No",
+      key: "SL No",
+    },
+
+    {
+      title: "Date",
+      dataIndex: "date",
+      key: "date",
+    },
+    {
+      title: "Particulars",
+      dataIndex: "particulars",
+      key: "particulars",
+    },
+    {
+      title: "Description",
+      dataIndex: "description",
+      key: "description",
+    },
+    {
+      title: "Quantity",
+      dataIndex: "quantity",
+      key: "quantity",
+    },
+    {
+      title: "Memo NO",
+      dataIndex: "memoNo",
+      key: "memoNo",
+    },
+    {
+      title: "Orderer By",
+      dataIndex: "ordererBy",
+      key: "ordererBy",
+    },
+    {
+      title: "Pay To",
+      dataIndex: "pay",
+      key: "pay",
+    },
+    {
+      title: "Payment Type",
+      dataIndex: "payment",
+      key: "payment",
+    },
+    {
+      title: "Unit",
+      dataIndex: "unit",
+      key: "unit",
+    },
+    {
+      title: "Unit Price",
+      dataIndex: "unit price",
+      key: "unit price",
+    },
+    {
+      title: "Total Price",
+      dataIndex: "total price",
+      key: "total price",
+    },
+    {
+      title: "Action",
+      key: "action",
+      render: (_: number, record: TFactory) => (
+        <Button type="link" onClick={() => navigate(`/product/${record.slNo}`)}>
+          View Details
+        </Button>
+      ),
+    },
+  ];
+  // const { data, isError, isLoading } = useGetTravellingsQuery({
+  //   limit: pageSize,
+  //   skip: (currentPage - 1) * pageSize,
+  // });
+
+  // if (isLoading) return <Loading />;
+  // if (isError) return <div>Error: {isError}</div>;
+  return (
+    <Table
+      className="table-auto"
+      bordered
+      columns={colums}
+      //   dataSource={data}
+      rowKey="id"
+      pagination={{
+        current: currentPage,
+        pageSize: pageSize,
+        //   total: data?.total,
+        onChange: (page, pageSize) => {
+          setCurrentPage(page);
+          setPageSize(pageSize);
+        },
+      }}
+    />
+  );
 };
 
 export default FactoryDevelopmentTable;
