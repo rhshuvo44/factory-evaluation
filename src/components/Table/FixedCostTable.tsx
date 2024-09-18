ec97dddb96093d802f3e655f3b9fd3ae835b1be0
Ripon Hossain Shuvo <rhshuvo44@gmail.com>
2024-09-18 14:28:58 +0600

    Fixed Cost table

--- src/components/Table/FixedCostTable.tsx
+++ src/components/Table/FixedCostTable.tsx
@@ -1,5 +1,71 @@
+import { Button, Table } from "antd";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useGetTravellingsQuery } from "../../redux/api/api";
+import { TFixed } from "../../types/tableType";
+import Loading from "../ui/Loading";
+
 const FixedCostTable = () => {
-  return <div></div>;
+  const [currentPage, setCurrentPage] = useState(1);
+  const [pageSize, setPageSize] = useState(5);
+  const navigate = useNavigate();
+
+  const colums = [
+    {
+      title: "Factory Rent",
+      dataIndex: "factoryRent",
+      key: "factoryRent",
+    },
+
+    {
+      title: "Honorary",
+      dataIndex: "honorary",
+      key: "honorary",
+    },
+    {
+      title: "Factory Revenue",
+      dataIndex: "factoryRevenue",
+      key: "factoryRevenue",
+    },
+
+    {
+      title: "Action",
+      key: "action",
+      render: (_: number, record: TFixed) => (
+        <Button
+          type="link"
+          onClick={() => navigate(`/product/${record.factoryRent}`)}
+        >
+          View Details
+        </Button>
+      ),
+    },
+  ];
+  const { data, isError, isLoading } = useGetTravellingsQuery({
+    limit: pageSize,
+    skip: (currentPage - 1) * pageSize,
+  });
+
+  if (isLoading) return <Loading />;
+  if (isError) return <div>Error: {isError}</div>;
+  return (
+    <Table
+      className="table-auto"
+      bordered
+      columns={colums}
+      //   dataSource={data}
+      rowKey="id"
+      pagination={{
+        current: currentPage,
+        pageSize: pageSize,
+        total: data?.total,
+        onChange: (page, pageSize) => {
+          setCurrentPage(page);
+          setPageSize(pageSize);
+        },
+      }}
+    />
+  );
 };
 
 export default FixedCostTable;
