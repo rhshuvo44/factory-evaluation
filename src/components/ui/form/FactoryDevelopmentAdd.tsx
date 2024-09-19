cdd00c83d575e25f7894b6daf2659a446a8d5caa
Ripon Hossain Shuvo <rhshuvo44@gmail.com>
2024-09-19 18:14:58 +0600

    Factory development  unit and unit price total price calculate system

--- src/components/ui/form/FactoryDevelopmentAdd.tsx
+++ src/components/ui/form/FactoryDevelopmentAdd.tsx
@@ -1,16 +1,40 @@
-import { Button, DatePicker, DatePickerProps, Form, Select } from "antd";
+import {
+  Button,
+  DatePicker,
+  DatePickerProps,
+  Form,
+  InputNumber,
+  InputNumberProps,
+  Select,
+} from "antd";
 import { TFactory } from "../../../types/tableType";
 import CustomInput from "../../form/CustomInput";
 import CustomInputNumber from "../../form/CustomInputNumber";
 import CustomTextArea from "../../form/CustomTextArea";
 import { formItemLayout } from "../../../constants/formItemLayout";
 import { paymentOptions } from "../../../constants/dropdownoptions";
+import { useEffect, useState } from "react";
 
 const FactoryDevelopmentAdd = () => {
-  let date: string;
+  const [form] = Form.useForm();
+  const [unit, setUnit] = useState<number>(0);
+  const [unitPrice, setUnitPrice] = useState<number>(0);
+  const [date, setDate] = useState<string | string[]>("");
+
   const onChangeDate: DatePickerProps["onChange"] = (_, dateString) => {
-    date = dateString as string;
+    setDate(dateString);
   };
+  const onChangeUnit: InputNumberProps["onChange"] = (values) => {
+    setUnit(values as number);
+  };
+  const onChangeUnitPrice: InputNumberProps["onChange"] = (values) => {
+    setUnitPrice(values as number);
+  };
+  useEffect(() => {
+    form.setFieldsValue({
+      totalPrice: unit * unitPrice,
+    });
+  }, [unit, unitPrice, form]);
   const onFinish = (values: TFactory) => {
     console.log("Received values of form: ", { ...values, date });
     // Call your backend API to handle the login request
@@ -18,7 +42,7 @@ const FactoryDevelopmentAdd = () => {
     // You can use the following code as a reference:
   };
   return (
-    <Form {...formItemLayout} onFinish={onFinish}>
+    <Form {...formItemLayout} onFinish={onFinish} form={form}>
       <CustomInputNumber
         label="SL No"
         name="slNo"
@@ -59,7 +83,6 @@ const FactoryDevelopmentAdd = () => {
       >
         <DatePicker onChange={onChangeDate} style={{ width: "100%" }} />
       </Form.Item>
-
       <Form.Item
         label="Payment Type"
         name="paymentType"
@@ -67,26 +90,37 @@ const FactoryDevelopmentAdd = () => {
       >
         <Select
           style={{ width: "100%" }}
-          defaultValue="monthly"
+          defaultValue="Please select Payment Type"
           options={paymentOptions}
         />
       </Form.Item>
-      <CustomInputNumber
+      <Form.Item
         label="Unit"
         name="unit"
-        message="Please input! Unit"
-      />
-      <CustomInputNumber
+        rules={[{ required: true, message: "Please Input Unit! " }]}
+      >
+        <InputNumber
+          style={{ width: "100%" }}
+          min={0}
+          onChange={onChangeUnit}
+        />
+      </Form.Item>
+
+      <Form.Item
         label="Unit Price"
         name="unitPrice"
-        message="Please input! Unit Price"
-      />
-      <CustomInputNumber
-        label="Total Price"
-        name="totalPrice"
-        message="Please input! Total Price"
-      />
+        rules={[{ required: true, message: "Please Input Unit Price! " }]}
+      >
+        <InputNumber
+          style={{ width: "100%" }}
+          min={0}
+          onChange={onChangeUnitPrice}
+        />
+      </Form.Item>
 
+      <Form.Item label="Total Price" name="totalPrice">
+        <InputNumber style={{ width: "100%" }} disabled />
+      </Form.Item>
       <Form.Item wrapperCol={{ offset: 6, span: 16 }}>
         <Button type="primary" htmlType="submit">
           Submit
