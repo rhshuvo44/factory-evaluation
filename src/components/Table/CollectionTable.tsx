bf1173d91845355b47ed500f02a30ad827ab4040
Ripon Hossain Shuvo <rhshuvo44@gmail.com>
2024-09-28 16:13:50 +0600

    Collection table

--- src/components/Table/CollectionTable.tsx
+++ src/components/Table/CollectionTable.tsx
@@ -1,18 +1,37 @@
-import { Button, Table } from "antd";
+import { Button, Space, Table } from "antd";
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { userRole } from "../../constants/userRole";
+import { useGetAllCollectionsQuery } from "../../redux/features/collection/collectionApi";
 import { TCollection } from "../../types/tableType";
+import Loading from "../ui/Loading";
+import { TUser, useCurrentToken } from "../../redux/features/auth/authSlice";
+import { useAppSelector } from "../../redux/hook";
+import { verifyToken } from "../../utilis/verifyToken";
 
 const CollectionTable = () => {
   const [currentPage, setCurrentPage] = useState(1);
   const [pageSize, setPageSize] = useState(5);
-  const navigate = useNavigate();
+  const token = useAppSelector(useCurrentToken);
+
+  let user;
+
+  if (token) {
+    user = verifyToken(token) as TUser;
+  }
+  // const [deleteBuyerDevelopment] = useDeleteBuyerDevelopmentMutation();
+  // const handleDeleted = async (id: string) => {
+  //   const res = await deleteBuyerDevelopment(id);
+  //   if (res.data.success) {
+  //     toast.success("Buyer Development deleted successfully.");
+  //   }
+  // };
 
   const colums = [
     {
       title: "SL No",
-      dataIndex: "SL No",
-      key: "SL No",
+      dataIndex: "slNo",
+      key: "slNo",
     },
 
     {
@@ -60,29 +79,47 @@ const CollectionTable = () => {
       dataIndex: "amount",
       key: "amount",
     },
-    {
-      title: "Action",
-      key: "action",
-      render: (_: number, record: TCollection) => (
-        <Button type="link" onClick={() => navigate(`/product/${record.slNo}`)}>
-          View Details
-        </Button>
-      ),
-    },
-  ];
-  // const { data, isError, isLoading } = useGetTravellingsQuery({
-  //   limit: pageSize,
-  //   skip: (currentPage - 1) * pageSize,
-  // });
 
-  // if (isLoading) return <Loading />;
-  // if (isError) return <div>Error: {isError}</div>;
+    ...(user?.role === userRole.ADMIN ||
+    user?.role === userRole.ExecutiveDirector
+      ? [
+          {
+            title: "Action",
+            key: "action",
+            render: (item: TCollection) => {
+              return (
+                <Space>
+                  <Link to={`/${user!.role}/travel_allowance/${item._id}`}>
+                    Edit
+                  </Link>
+                  {user!.role === "admin" && (
+                    <Button
+                      danger
+                      // onClick={() => handleDeleted(item._id as string)}
+                    >
+                      Delete
+                    </Button>
+                  )}
+                </Space>
+              );
+            },
+          },
+        ]
+      : []),
+  ];
+  const { data, isError, isLoading } = useGetAllCollectionsQuery({
+    limit: pageSize,
+    skip: (currentPage - 1) * pageSize,
+  });
+
+  if (isLoading) return <Loading />;
+  if (isError) return <div>Error: {isError}</div>;
   return (
     <Table
       className="table-auto"
       bordered
       columns={colums}
-      //   dataSource={data}
+      dataSource={data?.data?.result}
       rowKey="id"
       pagination={{
         current: currentPage,
