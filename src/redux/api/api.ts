0cad21e40f4eef7b572ea744768d9e0ee6a51c97
Ripon Hossain Shuvo <rhshuvo44@gmail.com>
2024-10-03 17:55:55 +0600

    redux api error issu fix

--- src/redux/api/api.ts
+++ src/redux/api/api.ts
@@ -35,12 +35,13 @@ const baseQueryWithRefreshToken: BaseQueryFn<
     DefinitionType
 > = async (args, api, extraOptions): Promise<any> => {
     let result = await baseQuery(args, api, extraOptions);
-
+    const { error } = result;
+    const {data}:any=error
     if (result?.error?.status === 404) {
-        toast.error(result.error?.data?.message);
+        toast.error(data?.message);
     }
     if (result?.error?.status === 403) {
-        toast.error(result.error?.data?.message)
+        toast.error(data?.message)
     }
     if (result?.error?.status === 401) {
         //* Send Refresh
