8c2c10681fb62ba2eb76f4cdb928b1fa92974f0b
Ripon Hossain Shuvo <rhshuvo44@gmail.com>
2024-10-19 14:38:45 +0600

    profile pages

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -2,7 +2,7 @@ import { useGetMeQuery } from "../redux/features/user/userApi";
 
 const Profile = () => {
   const { data } = useGetMeQuery(undefined);
- 
+
   return (
     <section className="w-72 mx-auto bg-[#20354b] rounded-2xl px-8 py-6 shadow-lg">
       <div className="mt-6 w-fit mx-auto">
@@ -18,24 +18,24 @@ const Profile = () => {
           {data?.data?.name}
         </h2>
         <p className="text-white">
-          <span className="font-bold"> username:</span> {data?.data?.username}
+          <span className="font-bold"> Username:</span> {data?.data?.username}
         </p>
         <p className="text-white">
-          <span className="font-bold"> email:</span> {data?.data?.email}
+          <span className="font-bold"> Email:</span> {data?.data?.email}
         </p>
         <p className="text-white">
-          <span className="font-bold"> phone:</span> {data?.data?.phone}
+          <span className="font-bold"> Phone:</span> {data?.data?.phone}
         </p>
         <p className="text-white">
-          <span className="font-bold"> role:</span> {data?.data?.role}
+          <span className="font-bold"> Role:</span> {data?.data?.role}
         </p>
         <p className="text-white">
-          <span className="font-bold"> address:</span> {data?.data?.address}
+          <span className="font-bold"> Address:</span> {data?.data?.address}
         </p>
       </div>
 
       <p className="text-emerald-400 font-semibold mt-2.5">
-        <span className="font-bold text-white"> status: </span>Active
+        <span className="font-bold text-white"> Status: </span>Active
       </p>
 
       <div className="h-1 w-full bg-black mt-8 rounded-full">
