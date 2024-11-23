46e8c97826215e95ef66253027cf58059b635b90
Ripon Hossain Shuvo <rhshuvo44@gmail.com>
2024-11-23 16:36:58 +0600

    Production report update

--- src/pages/Production/ProductionUpdate.tsx
+++ src/pages/Production/ProductionUpdate.tsx
@@ -1,4 +1,13 @@
-import { Button, DatePicker, Form, Input, InputNumber, Select } from "antd";
+import {
+  Button,
+  DatePicker,
+  Form,
+  Input,
+  InputNumber,
+  InputNumberProps,
+} from "antd";
+import moment from "moment";
+import { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import { toast } from "sonner";
 import CustomInput from "../../components/form/CustomInput";
@@ -15,22 +24,136 @@ const ProductionUpdate = () => {
   const [form] = Form.useForm();
   const location = useLocation();
   const navigate = useNavigate();
-
+  const [inHouse, setInHouse] = useState<number>(0);
+  const [cuttingCompleted, setCuttingCompleted] = useState<number>(0);
+  const [printCompleted, setPrintCompleted] = useState<number>(0);
+  const [deliveryToPrint, setDeliveryToPrint] = useState<number>(0);
+  const [sewingInput, setSewingInput] = useState<number>(0);
+  const [sewingOutput, setSewingOutput] = useState<number>(0);
+  const [finishingOutput, setFinishingOutput] = useState<number>(0);
+  const [packingCompleted, setPackingCompleted] = useState<number>(0);
   const id: string = location.pathname.split("/")[3];
   const { data, isLoading } = useSingleProductionQuery(id);
   const [updateProduction] = useUpdateProductionMutation();
-  console.log(data);
-  // const initialValues = {
-  //   date: data?.data?.date,
-  //   lineNo: data?.data?.lineNo,
-  //   buyer: data?.data?.buyer,
-  //   orderNo: data?.data?.orderNo,
-  //   styleNo: data?.data?.styleNo,
-  //   color: data?.data?.color,
-  //   orderQuantity: data?.data?.orderQuantity,
-  //   readyQuantity: data?.data?.readyQuantity,
-  //   remark: data?.data?.remark,
-  // };
+  const production = data?.data;
+  const inHouseChangeHandler: InputNumberProps["onChange"] = (values) => {
+    setInHouse(values as number);
+  };
+  const cuttingCompletedChangeHandler: InputNumberProps["onChange"] = (
+    values
+  ) => {
+    setCuttingCompleted(values as number);
+  };
+  const printCompletedChangeHandler: InputNumberProps["onChange"] = (
+    values
+  ) => {
+    setPrintCompleted(values as number);
+  };
+  const deliveryToPrintChangeHandler: InputNumberProps["onChange"] = (
+    values
+  ) => {
+    setDeliveryToPrint(values as number);
+  };
+  const sewingInputChangeHandler: InputNumberProps["onChange"] = (values) => {
+    setSewingInput(values as number);
+  };
+  const sewingOutputChangeHandler: InputNumberProps["onChange"] = (values) => {
+    setSewingOutput(values as number);
+  };
+  const finishingOutputChangeHandler: InputNumberProps["onChange"] = (
+    values
+  ) => {
+    setFinishingOutput(values as number);
+  };
+  const packingCompletedChangeHandler: InputNumberProps["onChange"] = (
+    values
+  ) => {
+    setPackingCompleted(values as number);
+  };
+
+  useEffect(() => {
+    if (production) {
+      form.setFieldsValue({
+        buyer: production?.buyer,
+        description: production?.description,
+        date: production?.date ? moment(production?.date) : undefined,
+        orderNo: production?.orderNo,
+        quantity: production?.quantity,
+        styleNo: production?.styleNo,
+        shipmentDate: production?.shipmentDate
+          ? moment(production?.shipmentDate)
+          : undefined,
+        leadTime: production?.leadTime,
+        fabricConsumption: production?.fabricConsumption,
+        totalFabric: production?.totalFabric,
+      });
+    }
+    const inHouseValue = inHouse || production?.fabricInHouse;
+    const requiredFabricCalculator =
+      production?.totalFabric - inHouseValue || production?.requiredFabric;
+    const cuttingCompletedValue =
+      cuttingCompleted || production?.cuttingCompleted;
+    const cuttingRequiredCalculator =
+      production?.quantity - cuttingCompletedValue ||
+      production?.cuttingRequired;
+    const deliveryToPrintValue = deliveryToPrint || production?.deliveryToPrint;
+    const deliveryToPrintRemainingCalculator =
+      cuttingCompletedValue - deliveryToPrintValue ||
+      production?.deliveryToPrintRemaining;
+    const printCompletedValue = printCompleted || production?.printCompleted;
+    const printReceivableCalculator =
+      deliveryToPrintValue - printCompletedValue || production?.printReceivable;
+    const sewingInputValue = sewingInput || production?.sewingInput;
+    const sewingInputRemainingCalculator =
+      printCompletedValue - sewingInputValue ||
+      production?.sewingInputRemaining;
+    const sewingOutputValue = sewingOutput || production?.sewingOutput;
+    const sewingOutputRemainingCalculator =
+      sewingInputValue - sewingOutputValue || production?.sewingOutputRemaining;
+
+    const finishingOutputValue = finishingOutput || production?.finishingOutput;
+    const finishingOutputRemainingCalculator =
+      sewingOutputValue - finishingOutputValue ||
+      production?.finishingOutputRemaining;
+    const packingCompletedValue =
+      packingCompleted || production?.packingCompleted;
+    const packingRemainingCalculator =
+      finishingOutputValue - packingCompletedValue ||
+      production?.packingRemaining;
+
+    form.setFieldsValue({
+      requiredFabric: requiredFabricCalculator,
+      cuttingRequired: cuttingRequiredCalculator,
+      deliveryToPrintRemaining: deliveryToPrintRemainingCalculator,
+      printReceivable: printReceivableCalculator,
+      sewingInputRemaining: sewingInputRemainingCalculator,
+      sewingOutputRemaining: sewingOutputRemainingCalculator,
+      finishingOutputRemaining: finishingOutputRemainingCalculator,
+      packingRemaining: packingRemainingCalculator,
+    });
+  }, [
+    form,
+    production,
+    inHouse,
+    cuttingCompleted,
+    deliveryToPrint,
+    printCompleted,
+    sewingInput,
+    sewingOutput,
+    finishingOutput,
+    packingCompleted,
+  ]);
+  const initialValues = {
+    fabricInHouse: production?.fabricInHouse,
+    cuttingCompleted: production?.cuttingCompleted,
+    printCompleted: production?.printCompleted,
+    deliveryToPrint: production?.deliveryToPrint,
+    sewingInput: production?.sewingInput,
+    sewingOutput: production?.sewingOutput,
+    finishingOutput: production?.finishingOutput,
+    packingCompleted: production?.packingCompleted,
+    remark: production?.remark,
+  };
   if (isLoading) return <Loading />;
   const onFinish = async (values: TProductionReport) => {
     const updateData = {
@@ -50,7 +173,7 @@ const ProductionUpdate = () => {
         onFinish={onFinish}
         form={form}
         layout="vertical"
-        // initialValues={initialValues}
+        initialValues={initialValues}
       >
         <Form.Item
           style={{ width: "100%" }}
@@ -58,15 +181,10 @@ const ProductionUpdate = () => {
           name="orderNo"
           rules={[{ required: true, message: "Please select Order No! " }]}
         >
-          <Select
-            showSearch
-            // onChange={orderNoChangeHandler}
-            placeholder="Please select order No! "
-            optionFilterProp="label"
-            // options={orders?.map((orderNo: string) => ({
-            //   value: orderNo,
-            //   label: orderNo,
-            // }))}
+          <InputNumber
+            style={{ width: "100%" }}
+            disabled
+            placeholder="please input order No!"
           />
         </Form.Item>
 
@@ -153,13 +271,12 @@ const ProductionUpdate = () => {
           label="Fabric In House (KG)"
           name="fabricInHouse"
           rules={[
-            {
-              required: true,
-              message: "Please Input Fabric In House (KG)",
-            },
+            { required: true, message: "Please Input Fabric In House (KG)" },
           ]}
         >
           <InputNumber
+            onChange={inHouseChangeHandler}
+            min={0}
             style={{ width: "100%" }}
             placeholder="please input Fabric In House (KG)"
           />
@@ -187,7 +304,10 @@ const ProductionUpdate = () => {
           ]}
         >
           <InputNumber
+            onChange={cuttingCompletedChangeHandler}
             style={{ width: "100%" }}
+            max={production?.quantity}
+            min={0}
             placeholder="Cutting Completed (Per price)"
           />
         </Form.Item>
@@ -219,6 +339,9 @@ const ProductionUpdate = () => {
         >
           <InputNumber
             style={{ width: "100%" }}
+            max={cuttingCompleted}
+            min={0}
+            onChange={deliveryToPrintChangeHandler}
             placeholder="Delivery To Print (Per Price)"
           />
         </Form.Item>
@@ -249,6 +372,9 @@ const ProductionUpdate = () => {
           ]}
         >
           <InputNumber
+            onChange={printCompletedChangeHandler}
+            min={0}
+            max={deliveryToPrint}
             style={{ width: "100%" }}
             placeholder="Print Completed"
           />
@@ -279,11 +405,17 @@ const ProductionUpdate = () => {
             },
           ]}
         >
-          <InputNumber style={{ width: "100%" }} placeholder="sewing Input" />
+          <InputNumber
+            min={0}
+            max={printCompleted}
+            onChange={sewingInputChangeHandler}
+            style={{ width: "100%" }}
+            placeholder="sewing Input"
+          />
         </Form.Item>
         <Form.Item
           label="sewing Input Remaining "
-          name="sewingInputRemaining "
+          name="sewingInputRemaining"
           rules={[
             {
               required: true,
@@ -307,11 +439,17 @@ const ProductionUpdate = () => {
             },
           ]}
         >
-          <InputNumber style={{ width: "100%" }} placeholder="sewing Output" />
+          <InputNumber
+            min={0}
+            max={sewingInput}
+            onChange={sewingOutputChangeHandler}
+            style={{ width: "100%" }}
+            placeholder="sewing Output"
+          />
         </Form.Item>
         <Form.Item
-          label="sewing Output Remaining "
-          name="sewingOutputRemaining "
+          label="sewing Output Remaining"
+          name="sewingOutputRemaining"
           rules={[
             {
               required: true,
@@ -338,12 +476,14 @@ const ProductionUpdate = () => {
           <InputNumber
             style={{ width: "100%" }}
             min={0}
+            max={sewingOutput}
+            onChange={finishingOutputChangeHandler}
             placeholder="Finishing Output"
           />
         </Form.Item>
         <Form.Item
           label="Finishing Output Remaining "
-          name="finishingOutputRemaining "
+          name="finishingOutputRemaining"
           rules={[
             {
               required: true,
@@ -370,12 +510,14 @@ const ProductionUpdate = () => {
           <InputNumber
             style={{ width: "100%" }}
             min={0}
+            max={finishingOutput}
+            onChange={packingCompletedChangeHandler}
             placeholder="Packing Completed"
           />
         </Form.Item>
         <Form.Item
           label="Packing Remaining "
-          name="packingRemaining "
+          name="packingRemaining"
           rules={[
             {
               required: true,
